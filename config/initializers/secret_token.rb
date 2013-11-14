64dc4fc82be012e3a90eeb7e4a3c7974679de2af
neversion <neversionchen@gmail.com>
2013-11-14 11:18:40 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XmuHydra::Application.config.secret_key_base = '1f8bf09c8017af660384b183adff38a0466780945752795a7b68661ec2ed958af863b8254c58dd58b1534e0560b3eaf97afb11888341070d1cc02f2c856da129'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XmuHydra::Application.config.session_store :cookie_store, key: '_xmu_hydra_session'
