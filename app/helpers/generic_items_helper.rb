f6b9f51cebc40321b5e405ccbc45649d6429a375
neversion <neversionchen@gmail.com>
2013-12-17 10:41:43 +0800

    规范字段中文名

--- app/helpers/generic_items_helper.rb
+++ app/helpers/generic_items_helper.rb
@@ -12,13 +12,13 @@ module GenericItemsHelper
         break
       end
     end
-    label_list = ["2.3.2 标题 Title proper", "2.3.6 副标题 Variant title", "副标题 Series statement",
+    label_list = ["2.3.2 题名 Title", "2.3.6 副标题 Variant title", "2.12 丛编说明 Series statement",
                   "关键字 Keyword", "19.2 创建者 Creator", "Set number", "索取号 Call Number", "6.9 内容类型 Content Type",
-                  "3.3 载体 Carrier type", "3.4 Extent Extent", "3.2 媒体类型 Media type", "7.10 简介 Summarization_of_the content",
+                  "3.3 载体类型 Carrier type", "3.4 数量/篇幅 Extent", "3.2 媒介类型 Media type", "7.10 内容摘要 Summarization_of_the content",
                   "2.8.4 出版社 Publisher’s name", "4.6 URL Uniform Resource Locator",
-                  "2.8.6 出版日期 Date of publication", "3.19.2 数字文件类型 Digital file type", "7.15 插图 Illustrative content",
-                  "7.22 时长 Duration", "3.5 尺寸 Dimensions", "7.23 Performer,Narrator,and/or Presenter",
-                  "Relationship designator", "20.2 Contributor", "7.12 语言", "上传时间"]
+                  "2.8.6 出版日期 Date of publication", "3.19.2 数字文件类型 Digital file type", "7.15 插图内容 Illustrative content",
+                  "7.22 持续时间 Duration", "3.5 尺寸 Dimensions", "7.23 出演者 Performer,Narrator,and/or Presenter",
+                  "Relationship designator", "20.2 贡献者 Contributor", "7.12 内容的语言 Language of content", "上传时间"]
     return label_list[temp_index]
 
   end
